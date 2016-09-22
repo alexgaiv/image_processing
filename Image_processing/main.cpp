b5af2b802e8e189e81148b8013e81cbe315ef3f8
Alexander Gaivanuk <alexgaiv0748@gmail.com>
2016-09-22 23:48:36 +0400

    tidy up, add smooth method

--- Image_processing/main.cpp
+++ Image_processing/main.cpp
@@ -13,9 +13,10 @@ TODO:
 -cleaning peaks under threshold
 */
 
+using namespace cv;
+using namespace std;
 
-int calculateIntensity(cv::Vec3b &pixel);
-void showImage(cv::Mat* image);
+uchar calculateIntensity(const cv::Vec3b &pixel);
 
 class Histogram {
 public:
@@ -23,21 +24,20 @@ public:
 	std::vector<int> vecMax;
 	std::vector<int> vecMin;
 
-	Histogram(cv::Mat* image) {
+	Histogram(const Mat &image) {
 		for (int i = 0; i < 254; i++) { hist[i] = 0; }
-		for (int i = 0; i < image->cols; i++) {
-			for (int j = 0; j < image->rows; j++) {
-				cv::Vec3b pixColor = image->at<cv::Vec3b>(cv::Point(i, j));
-				int intensity = calculateIntensity(pixColor);
+		for (int i = 0; i < image.cols; i++) {
+			for (int j = 0; j < image.rows; j++) {
+				cv::Vec3b pixColor = image.at<cv::Vec3b>(cv::Point(i, j));
+				uchar intensity = calculateIntensity(pixColor);
 				hist[intensity]++;
 			}
 		}
 	}
 
-	~Histogram() {
+	~Histogram() { }
 
-	}
-	void showHistorgam() const {
+	void showHistorgam(const char *windowName) const {
 		int hist_w = 300;
 		int hist_h = 200;
 		int histSize = 255;
@@ -46,7 +46,7 @@ public:
 		for (int i = 0; i < histSize - 1; i++){
 			line(histImage, cv::Point(i, hist_h), cv::Point(i, hist_h - hist[i] / histSize), cv::Scalar(255, 255, 255), 1, 8, 0);
 		}
-		showImage(&histImage);
+		imshow(windowName, histImage);
 	}
 
 	void searchLocalMax() {
@@ -66,7 +66,7 @@ public:
 			std::cout << i << std::endl;
 		}
 	}
-	
+
 	void searchLocalMin() {
 		if (hist[0] < hist[1])
 			vecMin.push_back(0);
@@ -85,20 +85,20 @@ public:
 		}
 	}
 
-	double calculatePeakMeasure(int &peakNum) const {
+	double calculatePeakMeasure(int peakNum) const {
 		double peakMesure = 0.0;
 		double square = 0.0;
 		if (peakNum == 0) {
 			for (int i = peakNum; i <= vecMin[0]; i++) {
 				square += hist[i];
 			}
-			peakMesure = (1 - hist[vecMin[0]] / (2 * hist[peakNum]))*(1 - square / ((vecMin[0] - peakNum+1)*hist[peakNum]));
+			peakMesure = (1 - hist[vecMin[0]] / (2 * hist[peakNum]))*(1 - square / ((vecMin[0] - peakNum + 1)*hist[peakNum]));
 		}
 		if (peakNum == 255) {
 			for (int i = vecMin[vecMin.size() - 1]; i <= peakNum; i++) {
 				square += hist[i];
 			}
-			peakMesure = (1 - hist[vecMin[vecMin.size() - 1]] / (2 * hist[peakNum]))*(1 - square / ((peakNum - vecMin[vecMin.size() - 1]+1)*hist[peakNum]));
+			peakMesure = (1 - hist[vecMin[vecMin.size() - 1]] / (2 * hist[peakNum]))*(1 - square / ((peakNum - vecMin[vecMin.size() - 1] + 1)*hist[peakNum]));
 		}
 		if (peakNum > 0 && peakNum < 255) {
 			int leftMin = 0;
@@ -110,44 +110,53 @@ public:
 			for (int i = vecMin[leftMin]; i <= vecMin[rightMin]; i++) {
 				square += hist[i];
 			}
-			peakMesure = (1 - (hist[vecMin[leftMin]] + hist[vecMin[rightMin]]) / (2 * hist[peakNum]))*(1 - square / ((vecMin[rightMin] - vecMin[leftMin]+1)*hist[peakNum]));
+			peakMesure = (1 - (hist[vecMin[leftMin]] + hist[vecMin[rightMin]]) / (2 * hist[peakNum]))*(1 - square / ((vecMin[rightMin] - vecMin[leftMin] + 1)*hist[peakNum]));
 		}
 		std::setprecision(3);
 		return peakMesure;
+	}
+
+	void smooth(int numPasses) { // сглаживание (см. презентацию)
+		int newHist[256] = { };
+
+		for (int k = 0; k < numPasses; k++)
+		{
+			for (int i = 1; i < 255; i++) {
+				newHist[i] = (hist[i - 1] + hist[i] + hist[i + 1]) / 3;
+			}
+			newHist[0] = (hist[0] + hist[1]) / 2;
+			newHist[255] = (hist[254] + hist[255]) / 2;
+			memcpy(hist, newHist, 256 * sizeof(int));
+		}
 	}
 };
 
 
 
-int main() {
-	cv::Mat image;
-	std::string filename = "test_Image.jpg";
-	image = cv::imread(filename);
-	//showImage(&image);
-	Histogram ImageHist(&image);
-	//ImageHist.showHistorgam();
-	ImageHist.searchLocalMax();
+int main()
+{
+	string filename = "test_Image.jpg";
+
+	Mat image = imread(filename);
+	imshow("Original Image", image);
+
+	Histogram imageHist(image);
+	imageHist.smooth(3);
+	imageHist.showHistorgam("Histogram");
+
+	/*ImageHist.searchLocalMax();
 	ImageHist.printLocalMax();
 	ImageHist.searchLocalMin();
 	ImageHist.printLocalMin();
 	std::cout << "VECMAX SIZE: " << ImageHist.vecMax.size() << std::endl;
 	for (int i = 0; i < ImageHist.vecMax.size(); i++) {
 		double tmp = ImageHist.calculatePeakMeasure(ImageHist.vecMax[i]);
-		std::cout << "PEAKMESURE OF " << i << " MAX: "<< std::setprecision(1) << tmp << std::endl;
-	}
-	exit(0);
-}
-
-int calculateIntensity(cv::Vec3b &pixel) {
-	int tmp = (int)(pixel[0] * 0.299 + pixel[1] * 0.587 + pixel[2] * 0.184);
-	if (tmp < 0) tmp = 0;
-	if (tmp > 255) tmp = 255;
-	return(tmp);
-}
-
-void showImage(cv::Mat* image) {
-	static char windowname[] = "Show";
-	cv::imshow(windowname, *image);
+		std::cout << "PEAKMESURE OF " << i << " MAX: " << std::setprecision(1) << tmp << std::endl;
+	}*/
 	cvWaitKey();
-	return;
+	return 0;
+}
+
+uchar calculateIntensity(const cv::Vec3b &pixel) {
+	return saturate_cast<uchar>(pixel[0] * 0.299 + pixel[1] * 0.587 + pixel[2] * 0.184);
 }
